72063a7f8c655c5f5e34e3c0f7af04eb231086ff
= <anand190900@gmail.com>
2024-05-29 15:02:52 +0530

    homepage almost completed

--- src/customer/components/HomeSectionCard/HomeSectionCard.jsx
+++ src/customer/components/HomeSectionCard/HomeSectionCard.jsx
@@ -1,18 +1,18 @@
 import React from "react";
 
-const HomeSectionCard = () => {
+const HomeSectionCard = ({product}) => {
   return (
-    <div className="cursor-pointer flex flex-col items-center bg-white rounded-lg shadow-lg overflow-hidden w-[15rem] mx-3">
+    <div className="cursor-pointer flex flex-col items-center bg-white rounded-lg shadow-lg overflow-hidden w-[13rem] mx-3 border">
       <div className="h-[13rem] w-[10rem]">
         <img
           className="object-cover object-top w-full h-full"
-          src="https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcTNOQZgrMh0dD0Ju8s9ANQ4k_3_BTqioAfVe4PzyVhd361yg57C52N9Ugi1MqkdmUn6bM6komGDtVkawcgaRDn2CRlOX-d61su5lldLqlfqzW7ZugMdrE2-bw"
+          src={product.imageUrl}
           alt=""
         />
       </div>
       <div className="p-4">
-      <h3 className="text-lg font-medium text-gray-900">HERE&NOW</h3>
-      <p className="mt-2 text-sm text-gray-500">Men Ethnic Motifs Embroidered Chikankari Kurta</p>
+      <h3 className="text-lg font-medium text-gray-900">{product.brand}</h3>
+      <p className="mt-2 text-sm text-gray-500">{product.title}</p>
       </div>
     </div>
   );
--- src/customer/components/HomeSectionCarousel/HomeSectionCarousel.jsx
+++ src/customer/components/HomeSectionCarousel/HomeSectionCarousel.jsx
@@ -1,24 +1,76 @@
-import React from "react";
+import React, { useState } from "react";
 import AliceCarousel from "react-alice-carousel";
 import HomeSectionCard from "../HomeSectionCard/HomeSectionCard";
+import KeyboardArrowLeftIcon from "@mui/icons-material/KeyboardArrowLeft";
+import { Button } from "@mui/material";
 
-const HomeSectionCarousel = () => {
+
+const HomeSectionCarousel = ({data, sectionName}) => {
+  const [activeIndex, setActiveIndex] = useState(0);
   const responsive = {
     0: { items: 1 },
     720: { items: 3 },
     1024: { items: 5.5 },
   };
 
-  const items = [1, 1, 1, 1, 1].map((item) => <HomeSectionCard />);
+  const slidePrev = () => setActiveIndex(activeIndex - 1);
+  const slideNext = () => setActiveIndex(activeIndex + 1);
+
+  const syncActiveIndex = ({ item }) => setActiveIndex(item);
+
+  const items = data
+    .slice(0, 10)
+    .map((item) => <HomeSectionCard product={item} />);
   return (
-    <div className="relative px-4 lg:px-8">
+    <div className="border">
+    <h2 className="text-2xl font-extrabold text-gray-800 py-5">{sectionName}</h2>
       <div className="relative p-5">
         <AliceCarousel
           items={items}
           disableButtonsControls
-          infinite
           responsive={responsive}
+          disableDotsControls
+          onSlideChanged={syncActiveIndex}
+          activeIndex={activeIndex}
         />
+        {activeIndex !== items.length - 1 && (
+          <Button
+            variant="contained"
+            className="z-50 bg-white"
+            onClick={slideNext}
+            sx={{
+              position: "absolute",
+              top: "8rem",
+              right: "0rem",
+              transform: "translateX(50%) rotate(90deg)",
+              bgcolor: "white",
+            }}
+            aria-label="next"
+          >
+            <KeyboardArrowLeftIcon
+              sx={{ transform: "rotate(90deg)", color: "black" }}
+            />
+          </Button>
+        )}
+        {activeIndex !== 0 && (
+          <Button
+            variant="contained"
+            className="z-50 bg-white"
+            onClick={slidePrev}
+            sx={{
+              position: "absolute",
+              top: "8rem",
+              left: "0rem",
+              transform: "translateX(-50%) rotate(-90deg)",
+              bgcolor: "white",
+            }}
+            aria-label="previous"
+          >
+            <KeyboardArrowLeftIcon
+              sx={{ transform: "rotate(90deg)", color: "black" }}
+            />
+          </Button>
+        )}
       </div>
     </div>
   );
